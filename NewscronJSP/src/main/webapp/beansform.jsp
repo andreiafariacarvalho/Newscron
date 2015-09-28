16de3e3f16d127e238f6701e822d742b7a5af982
DinTamari <Din@adminisatorsmbp.mobile.usilu.net>
2015-09-28 15:49:20 +0200

    Encryption version

---
+++ NewscronJSP/src/main/webapp/beansform.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : beansform
+    Created on : Sep 25, 2015, 4:25:44 PM
+    Author     : Din
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<body>
+<form method=POST action="NextPage.jsp">
+CustomerID: <input type=text name=custID><br>
+Reward1: <input type=text name=rew1><br>
+Reward2: <input type=text name=rew2><br>
+Validity: <input type=text name=val>
+<p><input type=submit>
+</form>
+</body>
+</html>
--- NewscronRefferal/src/ch/newscron/encryption/Encryption.java
+++ NewscronRefferal/src/ch/newscron/encryption/Encryption.java
@@ -7,17 +7,51 @@ package ch.newscron.encryption;
 
 /**
  *
- * @author andreiafariacarvalho
+ * @author andreiafariacarvalho + dintamari
  */
+
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.Cipher;
+import org.apache.commons.codec.binary.Base64;
+
 public class Encryption {
     
-    public static String encode(String word, String key) {
-        // TODO
+    private static final byte[] key = "newscron12345678".getBytes(); //Key for AES - multiple of 16bytes. Generate random?
+    
+    /**
+     * 
+     * @param params String that includes id, rew1, rew2, validity
+     * @return encoded string
+     */
+    public static String encode(String params) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            final SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.encodeBase64URLSafeString(cipher.doFinal(params.getBytes())); //to ensure valid URL characters
+        }
+        catch (Exception e) {
+           e.printStackTrace();
+        }
         return null;
     }
-    
-    public static String decode(String url, String key) {
-        // TODO
+
+    /**
+     * 
+     * @param encodedUrl 
+     * @return decoded String
+     */
+    public static String decode(String encodedUrl) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            final SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.decodeBase64(encodedUrl)));
+        }
+        catch (Exception e) {
+          e.printStackTrace();
+        }
         return null;
     }
 }
